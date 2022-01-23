88266b2bb96ebad1383f1074ed41ca217c9e70fc
Ashwyn D. Horton <ashwynh21@gmail.com>
2022-01-23 09:48:26 +0200

    feat: added prettier and rive as well as angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { RiveModule } from 'ng-rive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, RiveModule],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
